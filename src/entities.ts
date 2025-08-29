155a7678512d5d19c7265d105802aefb5a519058
hos1miya <113900368+hos1miya@users.noreply.github.com>
2025-08-29 16:06:01 +0900

    v1.0.8
    types

--- src/entities.ts
+++ src/entities.ts
@@ -333,10 +333,6 @@ export type LiteInstanceMetadata = {
 	repositoryUrl: string;
 	feedbackUrl: string;
 	disableRegistration: boolean;
-	disableLocalTimeline: boolean;
-	disableGlobalTimeline: boolean;
-	driveCapacityPerLocalUserMb: number;
-	driveCapacityPerRemoteUserMb: number;
 	emailRequiredForSignup: boolean;
 	enableHcaptcha: boolean;
 	hcaptchaSiteKey: string | null;
@@ -377,11 +373,7 @@ export type LiteInstanceMetadata = {
     avatarDecorationLimit: number,
 	};
 	enableEmail: boolean;
-	enableTwitterIntegration: boolean;
-	enableGithubIntegration: boolean;
-	enableDiscordIntegration: boolean;
 	enableServiceWorker: boolean;
-	emojis: CustomEmoji[];
 	defaultDarkTheme: string | null;
 	defaultLightTheme: string | null;
 	ads: {
@@ -392,6 +384,7 @@ export type LiteInstanceMetadata = {
 		imageUrl: string;
 	}[];
 	translatorAvailable: boolean;
+	mediaProxy: string;
 };
 
 export type DetailedInstanceMetadata = LiteInstanceMetadata & {
@@ -400,7 +393,17 @@ export type DetailedInstanceMetadata = LiteInstanceMetadata & {
 	cacheRemoteFiles: boolean;
 	requireSetup: boolean;
 	proxyAccountName: string | null;
-	features: TODO;
+	features: {
+		registration: boolean,
+		emailRequiredForSignup: boolean,
+		elasticsearch: boolean,
+		hcaptcha: boolean,
+		recaptcha: boolean,
+		turnstile: boolean,
+		objectStorage: boolean,
+		serviceWorker: boolean,
+		miauth: boolean,
+	};
 };
 
 export type InstanceMetadata = LiteInstanceMetadata | DetailedInstanceMetadata;
